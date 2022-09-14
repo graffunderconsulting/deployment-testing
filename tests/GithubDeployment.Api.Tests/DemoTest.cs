dab8dff6252983549e8f1dc9d9731b566c0fe686
Matt Graffunder <MattGraffunder@users.noreply.github.com>
2022-09-14 17:52:26 -0500

    Fix Setup

---
+++ tests/GithubDeployment.Api.Tests/DemoTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GithubDeployment.Api.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void Demo_Test()
+        {
+            Assert.True(true);
+        }
+    }
+}

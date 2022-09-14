637a613b80623c1ae1ee7df5aec313505a48b741
Matt Graffunder <MattGraffunder@users.noreply.github.com>
2022-09-14 17:35:44 -0500

    Add Base API

---
+++ src/GithubDeployement.Api/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubDeployement.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {        
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Test")]
+        public IActionResult Get([FromQuery] string? testName)
+        {
+            return Ok($"Hello {testName}");
+        }
+    }
+}
